bfed6357a416892ec551443f0fe1bdf048bd8cdd
abbasaghebaty <abbas.aghebaty@gmail.com>
2026-07-25 18:09:07 +0330

    Update products.js

--- src/routes/products.js
+++ src/routes/products.js
@@ -1,1 +1,25 @@
-test
+import { success } from "../utils/response.js";
+import { getProducts } from "../services/productService.js";
+
+
+export async function productsRouter(request, env) {
+
+
+  const url = new URL(request.url);
+
+
+  if (
+    request.method === "GET" &&
+    url.pathname === "/api/v1/products"
+  ) {
+
+    const products = await getProducts(env.DB);
+
+    return success(products);
+
+  }
+
+
+  return null;
+
+}

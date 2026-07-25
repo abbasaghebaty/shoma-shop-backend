2d907236c381492f09dfcbd91503c011764a9e22
abbasaghebaty <abbas.aghebaty@gmail.com>
2026-07-25 18:08:36 +0330

    Update productService.js

--- src/services/productService.js
+++ src/services/productService.js
@@ -1,1 +1,26 @@
-t
+export async function getProducts(DB) {
+
+  const { results } = await DB
+    .prepare(
+      `
+      SELECT 
+      products.*,
+      brands.name AS brand_name,
+      categories.name AS category_name
+
+      FROM products
+
+      LEFT JOIN brands 
+      ON products.brand_id = brands.id
+
+      LEFT JOIN categories
+      ON products.category_id = categories.id
+
+      ORDER BY products.id DESC
+      `
+    )
+    .all();
+
+
+  return results;
+}

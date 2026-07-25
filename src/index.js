7dc0bcf1436586416ddb678802f500e7f40d447d
abbasaghebaty <abbas.aghebaty@gmail.com>
2026-07-25 18:09:19 +0330

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,50 @@
+import { productsRouter } from "./routes/products.js";
+import { error } from "./utils/response.js";
+
+
 export default {
-  async fetch(request, env) {
 
-    const result = await env.DB
-      .prepare("SELECT 1 as test")
-      .first();
+async fetch(request, env) {
 
-    return Response.json(result);
+
+  try {
+
+
+    const productResponse =
+      await productsRouter(request, env);
+
+
+    if(productResponse){
+      return productResponse;
+    }
+
+
+
+    return error(
+      "Route not found",
+      404
+    );
+
+
+  } catch(error){
+
+
+    return new Response(
+      JSON.stringify({
+        success:false,
+        error:error.message
+      }),
+      {
+        status:500,
+        headers:{
+          "Content-Type":"application/json"
+        }
+      }
+    );
+
 
   }
+
 }
+
+};

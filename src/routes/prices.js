acefb83e63f794a1ec0f1539ee5e8a935609194d
abbasaghebaty <abbas.aghebaty@gmail.com>
2026-07-25 18:43:08 +0330

    Update prices.js

--- src/routes/prices.js
+++ src/routes/prices.js
@@ -1,1 +1,112 @@
-ف
+import { success, error } from "../utils/response.js";
+
+import {
+getProductPrice,
+updateProductPrice,
+addPriceCheck
+
+} from "../services/priceService.js";
+
+
+
+export async function pricesRouter(request, env){
+
+
+const url = new URL(request.url);
+
+
+
+/*
+GET PRICE
+/api/v1/products/1/prices
+*/
+
+if(
+request.method==="GET" &&
+url.pathname.match(/^\/api\/v1\/products\/\d+\/prices$/)
+){
+
+
+const id =
+url.pathname.split("/")[4];
+
+
+const price =
+await getProductPrice(env.DB,id);
+
+
+
+return success(price);
+
+}
+
+
+
+
+
+/*
+UPDATE PRICE
+*/
+
+if(
+request.method==="PUT" &&
+url.pathname.match(/^\/api\/v1\/products\/\d+\/prices$/)
+){
+
+const id =
+url.pathname.split("/")[4];
+
+
+const body =
+await request.json();
+
+
+await updateProductPrice(
+env.DB,
+id,
+body
+);
+
+
+return success({
+message:"price updated"
+});
+
+
+}
+
+
+
+
+
+/*
+PRICE CHECK
+*/
+
+if(
+request.method==="POST" &&
+url.pathname.match(/^\/api\/v1\/products\/\d+\/price-check$/)
+){
+
+const id =
+url.pathname.split("/")[4];
+
+
+await addPriceCheck(
+env.DB,
+id,
+false
+);
+
+
+
+return success({
+message:"price checked"
+});
+
+}
+
+
+return null;
+
+}

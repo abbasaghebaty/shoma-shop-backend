f68501a891c74df3f26ecfdad6fdd7f821cb8cdf
abbasaghebaty <abbas.aghebaty@gmail.com>
2026-07-25 19:07:48 +0330

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import { adminProductsRouter } from "./routes/admin-products.js";
 import { customersRouter } from "./routes/customers.js";
 import { ordersRouter } from "./routes/orders.js";
 import { paymentsRouter } from "./routes/payments.js";
+import { dashboardRouter } from "./routes/dashboard.js";
 
 import { error } from "./utils/response.js";
 
@@ -15,7 +16,6 @@ async fetch(request, env) {
 
 
 const url = new URL(request.url);
-
 
 
 // Health Check
@@ -37,119 +37,119 @@ return Response.json({
 
 
 
-// Products
+// Products API
 
 const productResponse =
 await productsRouter(request, env);
 
 
 if(productResponse){
-
     return productResponse;
-
 }
 
 
 
 
 
-// Prices
+// Prices API
 
 const priceResponse =
 await pricesRouter(request, env);
 
 
 if(priceResponse){
-
     return priceResponse;
-
 }
 
 
 
 
 
-// Inventory
+// Inventory API
 
 const inventoryResponse =
 await inventoryRouter(request, env);
 
 
 if(inventoryResponse){
-
     return inventoryResponse;
-
 }
 
 
 
 
 
-// Admin Products
+// Admin Products API
 
 const adminProductResponse =
 await adminProductsRouter(request, env);
 
 
 if(adminProductResponse){
-
     return adminProductResponse;
-
 }
 
 
 
 
 
-// Customers
+// Customers API
 
 const customersResponse =
 await customersRouter(request, env);
 
 
 if(customersResponse){
-
     return customersResponse;
-
 }
 
 
 
 
 
-// Orders
+// Orders API
 
 const ordersResponse =
 await ordersRouter(request, env);
 
 
 if(ordersResponse){
-
     return ordersResponse;
-
 }
 
 
 
 
 
-// Payments
+// Payments API
 
 const paymentsResponse =
 await paymentsRouter(request, env);
 
 
 if(paymentsResponse){
-
     return paymentsResponse;
-
 }
 
 
 
 
 
-// Not Found
+// Dashboard API
+
+const dashboardResponse =
+await dashboardRouter(request, env);
+
+
+if(dashboardResponse){
+    return dashboardResponse;
+}
+
+
+
+
+
+// Route not found
 
 return error(
 "Route not found",
@@ -157,8 +157,6 @@ return error(
 );
 
 
-
 }
-
 
 };

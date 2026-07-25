65647131d316fc4a2a1ea0815ab1146a4e3a482e
abbasaghebaty <abbas.aghebaty@gmail.com>
2026-07-25 20:54:54 +0330

    Create auth.js

---
+++ src/routes/auth.js
@@ -0,0 +1,135 @@
+import { verifyPassword, generateToken } from "../utils/auth.js";
+import { error } from "../utils/response.js";
+
+
+export async function authRouter(request, env){
+
+const url = new URL(request.url);
+
+
+// LOGIN
+
+if(
+request.method === "POST" &&
+url.pathname === "/api/v1/auth/login"
+){
+
+
+const body = await request.json();
+
+
+const username = body.username;
+const password = body.password;
+
+
+
+if(!username || !password){
+
+return error(
+"Username and password required",
+400
+);
+
+}
+
+
+
+// پیدا کردن ادمین
+
+const admin = await env.DB
+.prepare(
+`
+SELECT *
+FROM admins
+WHERE username = ?
+AND is_active = 1
+`
+)
+.bind(username)
+.first();
+
+
+
+if(!admin){
+
+return error(
+"Invalid credentials",
+401
+);
+
+}
+
+
+
+// بررسی رمز
+
+const valid =
+await verifyPassword(
+password,
+admin.password_hash
+);
+
+
+
+if(!valid){
+
+return error(
+"Invalid credentials",
+401
+);
+
+}
+
+
+
+// ساخت session token
+
+const token =
+generateToken();
+
+
+
+await env.DB
+.prepare(
+`
+INSERT INTO admin_sessions
+(
+admin_id,
+token
+)
+VALUES
+(?,?)
+`
+)
+.bind(
+admin.id,
+token
+)
+.run();
+
+
+
+return Response.json({
+
+success:true,
+
+token,
+
+admin:{
+id:admin.id,
+username:admin.username,
+name:admin.name,
+role:admin.role
+}
+
+});
+
+
+}
+
+
+
+return null;
+
+
+}

74ad3ed717bddfbc9cc504bd1af858eaec068adc
abbasaghebaty <abbas.aghebaty@gmail.com>
2026-07-25 18:08:17 +0330

    Update response.js

--- src/utils/response.js
+++ src/utils/response.js
@@ -1,1 +1,30 @@
-t
+export function success(data, status = 200) {
+  return new Response(
+    JSON.stringify({
+      success: true,
+      data
+    }),
+    {
+      status,
+      headers: {
+        "Content-Type": "application/json"
+      }
+    }
+  );
+}
+
+
+export function error(message, status = 400) {
+  return new Response(
+    JSON.stringify({
+      success: false,
+      error: message
+    }),
+    {
+      status,
+      headers: {
+        "Content-Type": "application/json"
+      }
+    }
+  );
+}

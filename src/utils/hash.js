996bc92ea0bcc41c7596effb01202303e9c0d8d1
abbasaghebaty <abbas.aghebaty@gmail.com>
2026-07-27 10:09:38 +0330

    Update hash.js

--- src/utils/hash.js
+++ src/utils/hash.js
@@ -1,16 +1,25 @@
 import bcrypt from "bcryptjs";
 
 
-export async function hashPassword(password){
+// ساخت هش رمز عبور (برای ذخیره در دیتابیس)
+export async function hashPassword(password) {
 
-    return await bcrypt.hash(password,10);
+    return await bcrypt.hash(password, 10);
 
 }
 
 
+// بررسی رمز عبور وارد شده با هش ذخیره شده
+export async function verifyPassword(password, hash) {
 
-export async function verifyPassword(password,hash){
+    return await bcrypt.compare(password, hash);
 
-    return await bcrypt.compare(password,hash);
+}
+
+
+// ساخت توکن یکتا برای session
+export function generateToken() {
+
+    return crypto.randomUUID() + "-" + crypto.randomUUID();
 
 }

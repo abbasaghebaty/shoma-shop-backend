7d3253d7ff7330e700c8c8706409bada875f3547
abbasaghebaty <abbas.aghebaty@gmail.com>
2026-07-27 10:09:14 +0330

    Update auth.js

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -1,25 +1,25 @@
 import bcrypt from "bcryptjs";
 
 
-// ساخت هش رمز
-export async function hashPassword(password){
+// ساخت هش رمز عبور (برای ذخیره در دیتابیس)
+export async function hashPassword(password) {
 
-    return await bcrypt.hash(password,10);
-
-}
-
-
-// بررسی رمز
-export async function verifyPassword(password,hash){
-
-    return await bcrypt.compare(password,hash);
+    return await bcrypt.hash(password, 10);
 
 }
 
 
-// ساخت توکن سشن
-export function generateToken(){
+// بررسی رمز عبور وارد شده با هش ذخیره شده
+export async function verifyPassword(password, hash) {
 
-    return crypto.randomUUID();
+    return await bcrypt.compare(password, hash);
+
+}
+
+
+// ساخت توکن یکتا برای session
+export function generateToken() {
+
+    return crypto.randomUUID() + "-" + crypto.randomUUID();
 
 }
